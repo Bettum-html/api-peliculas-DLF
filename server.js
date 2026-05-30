1d9b323a23e94f01d3739e74e434708a3f4bda9b
Bettum|.| <d.leon24@info.uas.edu.mx>
2026-05-29 21:38:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,32 +3,22 @@ import { sequelize } from './modules/db.js';
 import peliculasRouter from './routes/ruta-pelicula.js';
 
 const app = express();
+
 app.use(express.json());
-
 // Inicialización asíncrona de la base de datos
-try {
-await sequelize.authenticate();
-console.log('Conexión con PostgreSQL establecida correctamente.');
-await sequelize.sync(); // Crea la tabla en la nube si no existe
-} catch (error) {
-console.error('Error al inicializar la base de datos:', error);
-}
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-app.use((req, res, next) => {
-  const apiKey = req.headers['x-api-key'];
-  if (apiKey === '12345') {
-    next();
-  } else {
-    res.status(403).json({ error: 'API Key inválida' });
+async function iniciarServidor() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión con PostgreSQL establecida correctamente.');
+    await sequelize.sync();// Crea la tabla en la nube si no existe
+    app.listen(process.env.PORT || 3001, () => {
+      console.log(`Servidor iniciado`);
+    });
+  } catch (error) {
+    console.error(error);
   }
-});
-
+}
 
 app.use('/peliculas', peliculasRouter);
 
-app.listen(process.env.PORT || 3001, () => console.log('API lista en http://localhost:3000'));
+iniciarServidor();
